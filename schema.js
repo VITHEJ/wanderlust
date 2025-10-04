2f4ac9557bd1d9d54efd0358cbc4c265f95df64e
vithej <vithejbhavna999@gmail.com>
2025-10-04 14:41:11 +0530

    erroe

--- schema.js
+++ schema.js
@@ -4,9 +4,9 @@ module.exports.ListingSchema = Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         image: Joi.string().allow('',null),
-        //price: Joi.number().required().min(0),
-        //location: Joi.string().required(),
-        //country: Joi.string().required()
+        price: Joi.number().required().min(0),
+        location: Joi.string().required(),
+        country: Joi.string().required()
     }).required()
             });
 
