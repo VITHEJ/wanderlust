d47d0aecdc0c8c9207184eeafd1f0f8501c6678f
vithej <vithejbhavna999@gmail.com>
2025-10-04 13:55:15 +0530

    price error

--- schema.js
+++ schema.js
@@ -4,7 +4,7 @@ module.exports.ListingSchema = Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         image: Joi.string().allow('',null),
-        price: Joi.number().required().min(0),
+        //price: Joi.number().required().min(0),
         location: Joi.string().required(),
         country: Joi.string().required()
     }).required()
